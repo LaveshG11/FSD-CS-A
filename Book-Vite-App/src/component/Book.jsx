ffb9d53b00fdaa4af64a1c20f95e55e440a1fa81
Lavesh Gupta <parul.varshney08@gmail.com>
2025-10-31 12:04:47 +0530

    React-Vite-App

---
+++ Book-Vite-App/src/component/Book.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Book = () => {
+  return (
+    <div className='card'>
+      <img src="" alt="Book image" width={200} height={200} />
+      <h3>Title : Physics</h3>
+      <h4>Price : ₹456/- </h4>
+      <br />
+      <button>Add To Cart</button>
+    </div>
+  )
+}
+
+export default Book
