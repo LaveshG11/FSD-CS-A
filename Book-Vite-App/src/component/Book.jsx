0eed39e6897d7c64228448e0b276722023f010ca
LaveshG11 <lavesh.23b0121010@abes.ac.in>
2025-10-31 12:28:34 +0530

    changes

--- Book-Vite-App/src/component/Book.jsx
+++ Book-Vite-App/src/component/Book.jsx
@@ -6,7 +6,6 @@ const Book = () => {
       <img src="" alt="Book image" width={200} height={200} />
       <h3>Title : Physics</h3>
       <h4>Price : ₹456/- </h4>
-      <br />
       <button>Add To Cart</button>
     </div>
   )
