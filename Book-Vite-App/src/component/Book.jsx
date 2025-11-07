35a40a340aa663a8379bcec3ef421866b2215bae
LaveshG11 <lavesh.23b0121010@abes.ac.in>
2025-11-07 12:47:04 +0530

    Shopping App

--- Book-Vite-App/src/component/Book.jsx
+++ Book-Vite-App/src/component/Book.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const Book = () => {
+const Book = ({book}) => {
   return (
     <div className='card'>
-      <img src="" alt="Book image" width={200} height={200} />
-      <h3>Title : Physics</h3>
-      <h4>Price : ₹456/- </h4>
+      <img src={book.image} alt="Book image" width={200} height={200} />
+      <h3>Title : {book.title}</h3>
+      <h4>Price : ₹ {book.price}/- </h4>
       <button>Add To Cart</button>
     </div>
   )
---
+++ Book-Vite-App/src/component/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import tailwind from 'tailwindcss'
+import './Footer.css'
+
+function Footer() {
+  return (
+    <div className='footer'>
+      <h3><center>Design By Lavesh Gupta</center></h3>
+    </div>
+  )
+}
+
+export default Footer
