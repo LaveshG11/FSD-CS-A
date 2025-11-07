adb86ecbb330682782a0e6f3885e18669a14bb1c
LaveshG11 <lavesh.23b0121010@abes.ac.in>
2025-11-07 13:01:30 +0530

    changes

--- Shopping-Vite-App/Client/src/pages/UserDashboard.jsx
+++ Shopping-Vite-App/Client/src/pages/UserDashboard.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from '../components/Header'
 import Navbar from '../components/Navbar'
 import { Outlet } from 'react-router-dom'
+import Footer from '../components/Footer'
 
 const UserDashboard = () => {
   return (
@@ -9,6 +10,7 @@ const UserDashboard = () => {
       <Header/>
       <Navbar/>
       <Outlet/>
+      <Footer/>
     </div>
   )
 }
