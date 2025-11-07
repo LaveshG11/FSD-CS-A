ec7d941eb5b23645b4623aa6d6706b1f8551394d
LaveshG11 <lavesh.23b0121010@abes.ac.in>
2025-11-07 13:13:29 +0530

    new changes

--- Shopping-Vite-App/Client/src/components/ShoppingItems.jsx
+++ Shopping-Vite-App/Client/src/components/ShoppingItems.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
-
+import React, { useState ,useEffect} from 'react'
+import Cart from './Cart';
+import './ShoppingItems.css'
 const ShoppingItems = () => {
+    const [cartItems,setCartItems]=useState([]);
+    const [error,setError]=useState(null);
+    useEffect(()=>{
+        fetch('https://fakestoreapi.com/products')
+        .then(res=>res.json())
+        .then(data=>{
+            setCartItems(data);
+        })
+        .catch(err=>{
+           setError(err.message)
+        })
+    },[])
   return (
-    <div>
-      <h1>Shopping Items</h1>
+    <div >
+        {error && <h1> Error: {error}</h1>}
+      <div className='items'>{
+        cartItems.map((b,i)=>
+            (<Cart key={i} item={b}/>))
+      }</div>
     </div>
   )
 }
